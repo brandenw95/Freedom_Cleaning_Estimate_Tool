11a8e4319f512321d45e95c2c05eb5957693d2d1
Branden <36615598+brandenw95@users.noreply.github.com>
2022-07-05 14:31:02 -0700

    Add files via upload

--- action_page.php
+++ action_page.php
@@ -1,6 +1,84 @@
-<?php
- 
-var_dump($_POST);
+<?php 
+    
+    //header( "refresh:10; url=https://google.ca" ); 
+    // Post data info
 
+    // Client Personal/Contact Info
+    $client_first_name = $_POST['firstname'];
+    $client_last_name = $_POST['lastname'];
+    $client_address = $_POST['address'];
+    $client_phone_number = $_POST['number'];
+    $client_email = $_POST['email'];
+    $client_message = $_POST['note'];
+    
+    // cleaning Service Selected
+    // Note: Strip end digits and underscores then add space between capitals using regex
+    $cleaning_service_raw = rtrim($_POST['clean_service'], '_0123456789');
+    $cleaning_service = preg_replace('/(?<!\ )[A-Z]/', ' $0', $cleaning_service_raw);
+    $cleaning_service_base_price = substr($_POST['clean_service'], -3);
+    
+    // Pet and Family addon selected (none - EZ - regular)
+    $pet_addon_service = $_POST['Pet_Package_Select'];
+    $family_addon_service = $_POST['Family_Package_Select'];
 
+    // Email to Freedom Cleaning and Organizing:
+    $to = 'sales@radixmedia.ca';
+    $subject = 'New client booking request from '.$client_first_name.' '.$client_last_name.'';
+    $headers = array('Content-Type: text/html; charset=UTF-8');
+    $message = '<br><br>
+        <b>Client Name:</b> '.$client_first_name.' '.$client_last_name.'<br>
+        <b>Client Phone Number:</b> '.$client_phone_number.'<br>
+        <b>Client Email Address:</b> '.$client_email.'<br>
+        <b>Base Package:</b> '.$cleaning_service.'<br>
+        <b>Base Package Price:</b> '.$cleaning_service_base_price.'<br>
+        <b>Pet Addon:</b> '.$pet_addon_service.' <br>
+        <b>Family Addon:</b> '.$family_addon_service.' <br>
+        <b>Addons:</b> <br>
+        <br>
+        <b>Total Estimated:</b> <br><br>
+        <b>Note From Client:</b> '.$client_message.'
+    ';
+    // wp_mail( $to, $subject, $message, $headers);
+
+    // Email to Client: (Disabled)
+    // $client_to = 'sales@radixmedia.ca';
+    // $client_subject = 'New client booking request from '.$client_first_name.' '.$client_last_name.'';
+    // $client_headers = array('Content-Type: text/html; charset=UTF-8');
+    // $client_message = '<br><br>
+    //     <b>Client Name:</b> '.$client_first_name.' '.$client_last_name.'<br>
+    //     <b>Client Phone Number:</b> '.$client_phone_number.'<br>
+    //     <b>Client Email Address:</b> '.$client_email.'<br>
+    //     <b>Base Package:</b> '.$cleaning_service.'<br>
+    //     <b>Base Package Price:</b> '.$cleaning_service_base_price.'<br>
+    //     <b>Pet Addon:</b> '.$pet_addon_service.' <br>
+    //     <b>Family Addon:</b> '.$family_addon_service.' <br>
+    //     <b>Addons:</b> <br>
+    //     <br>
+    //     <b>Total Estimated:</b> <br><br>
+    //     <b>Note From Client:</b> '.$client_message.'
+    // ';
+    // wp_mail( $client_to, $client_subject, $client_message, $client_headers);
+
+    echo '<head>
+        <link rel="stylesheet" type="text/css" href="style.css"/>
+        <link rel="stylesheet" type="text/css" href="//fonts.googleapis.com/css?family=Quicksand" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+        <script src="script.js"></script>
+        <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" type="text/css" href="multi-select.css">
+    </head>';
+        
+
+    echo '<section class="confirm-section">';
+        echo '<img src="bg8.jpg">';
+        echo '<div class="confirm-columns">';
+        echo '<div class="confirm-column-header"><h2>Thank you!</h2></div>';
+            echo '<div class="confirm-column-content">';
+                echo '<img class="checkmark" src="whitecheck.png">';
+                echo '<p><b>Your message has been recieved and we will contact you within 24 hours.<b></p>';
+                echo '<a href="https://freedomcleaning.ca"><button class="confirm-button">OK</button></a>';
+                echo '<h5><b>(You will be redirected to the main page automatically)</b></h5>';
+            echo '</div>';
+        echo '</div>';
+    echo '</section>';
 ?>
--- script.js
+++ script.js
@@ -1,109 +1,135 @@
 $(document).ready(function() {
-    $("div.desc").hide();
-    $("#EZ_Family_Addons").hide();
-    $("#Regular_Family_Addons").hide();
-    $("#EZ_Pet_Addons").hide();
-    $("#Regular_Pet_Addons").hide();
-    $('#cost').text('$0 - $0');
-    var counter = 0;
-    var addons = [];
 
-    $("input[name='clean_service']").click(function() {
-        
-        // Hide show the relative addons
-        var button_value = $(this).val();
-        $("div.desc").slideUp();
-        $("p.default_text").hide();
-        $("#" + button_value).delay(50).slideDown();
-      
-        // Update base price of package with Low-High Price
-        var clean_cost = button_value.slice(button_value.length - 3);
+  /* Initialization */
+  $("div.desc").hide();
+  $("#EZ_Family_Addons").hide();
+  $("#Regular_Family_Addons").hide();
+  $("#EZ_Pet_Addons").hide();
+  $("#Regular_Pet_Addons").hide();
+  $('#cost').text('$0 - $0');
 
-        // Update header with appropriate Information
-        option_header = button_value.split("_");
-        option_header = option_header[0];
-
-        if(option_header == 'LittleBitClean'){
-          $('#options-header').text("Addons (Please Choose 3)");
-        }
-        if(option_header == 'ClassicClean'){
-          $('#options-header').text("Includes:");
-        }
-        if(option_header == 'DiamondClean'){
-          $('#options-header').text("Includes:");
-        }
-        if(option_header == 'HelloFreedom'){
-          $('#options-header').text("Includes:");
-        }
-        if(option_header == 'GirlfriendPackage'){
-          $('#options-header').text("Includes:");
-        }
-        if(option_header == 'MoveOutClean'){
-          $('#options-header').text("Includes:");
-        }
-        if(option_header == 'InThePatch'){
-          $('#options-header').text("Includes:");
-        }
-        
-        $("select").on('change',function () {
-
-         
-          var addon_price = $(this).find(':selected').data('price');
-          
-           console.log(clean_cost);
-           clean_cost = parseFloat(clean_cost) + parseFloat(addon_price);
-           console.log(clean_cost);
-
-           var low = parseFloat(clean_cost);
-           var high = parseFloat(clean_cost) + parseFloat((clean_cost * 0.2));
-           $('#cost').text("$" + low + " - " + "$" + high);
-           counter++;
-        });
-        
-        // Calculate high and low
-        var low = parseFloat(clean_cost);
-        var high = parseFloat(clean_cost) + parseFloat((clean_cost * 0.2));
-        //$('#cost').text("$" + low + " - " + "$" + high);
-        
-        // initial package select 
-        $('#cost').text("$" + low);
-    });
-
-    // Family addon functionality
-    $("#family_package_dropdown").on('change', function(){
-      if(this.value == 'EZ Family'){
-        $("#EZ_Family_Addons").slideDown();
-        $("#Regular_Family_Addons").hide();
-      }
-      else if(this.value == 'Regular Family'){
-        $("#Regular_Family_Addons").slideDown();
-        $("#EZ_Family_Addons").hide();
-
-      }
-      else{
-        $("#EZ_Family_Addons").hide();
-        $("#Regular_Family_Addons").hide();
-      }
-    });
-
-    // Pet addon functionality
-    $("#pet_package_dropdown").on('change', function(){
-      if(this.value == 'EZ Pet'){
-        $("#EZ_Pet_Addons").slideDown();
-        $("#Regular_Pet_Addons").hide();
-      }
-      else if(this.value == 'Regular Pet'){
-        $("#Regular_Pet_Addons").slideDown();
-        $("#EZ_Pet_Addons").hide();
-
-      }
-      else{
-        $("#EZ_Pet_Addons").hide();
-        $("#Regular_Pet_Addons").hide();
-      }
-    });
-
+  /* REGULAR HELPER FUNCTIONS */
+  function add_20_percent(cost){
   
+    var final_total = parseFloat(cost) + parseFloat(cost * 0.2);
+    return final_total 
+  }
+
+  function update_display_price(cost){
+
+    var current_price = $('#cost').val();
+
+    if(isNaN(cost)){
+      console.log(cost);
+      console.log(typeof(cost));
+      console.log(parseFloat(cost));
+
+    }
+    if(Array.isArray(cost) ){
+
+      var sum_of_array = 0;
+      for(let i = 0; i < cost.length; i++){
+        sum_of_array += cost[i];
+      }
+      var sanitized_cost = parseFloat(sum_of_array);
+      var cost_low = sanitized_cost;
+      var cost_high = add_20_percent(sanitized_cost);
+    }
+    else{
+
+      var sanitized_cost = parseFloat(cost);
+      var cost_low = sanitized_cost;
+      var cost_high = add_20_percent(sanitized_cost);
+    }
+
+    $('#cost').text("$" + cost_low + " - " + "$" + cost_high);
+  }
+
+  /* EVENT FUNCTIONS */
+  $("input[name='clean_service']").click(function() {
+
+    // Initial Package select
+    // Hide show the relative addons
+    var button_value = $(this).val();
+    $("div.desc").slideUp();
+    $("p.default_text").hide();
+    $("#" + button_value).delay(50).slideDown();
+  
+    // Update base price of package with Low-High Price
+    var clean_cost = button_value.slice(button_value.length - 3);
+
+    // Update header with appropriate Information
+    option_header = button_value.split("_");
+    option_header = option_header[0];
+
+    if(option_header == 'LittleBitClean'){
+      $('#options-header').text("Addons (Please Choose 3)");
+    }
+    if(option_header == 'ClassicClean'){
+      $('#options-header').text("Includes:");
+    }
+    if(option_header == 'DiamondClean'){
+      $('#options-header').text("Includes:");
+    }
+    if(option_header == 'HelloFreedom'){
+      $('#options-header').text("Includes:");
+    }
+    if(option_header == 'GirlfriendPackage'){
+      $('#options-header').text("Includes:");
+    }
+    if(option_header == 'MoveOutClean'){
+      $('#options-header').text("Includes:");
+    }
+    if(option_header == 'InThePatch'){
+      $('#options-header').text("Includes:");
+    }
+    
+    $("select").on('change',function () {
+      var addon_price = $(this).find(':selected').data('price');
+        clean_cost = parseFloat(clean_cost) + parseFloat(addon_price);
+        var low = parseFloat(clean_cost);
+        update_display_price(low);
+        
+    });
+    // Calculate high and low
+    var low = parseFloat(clean_cost);
+    $('#cost').text("$" + low);
+  });
+
+
+  // Family addon functionality
+  $("#family_package_dropdown").on('change', function(){
+    if(this.value == 'EZ Family'){
+      $("#EZ_Family_Addons").slideDown();
+      $("#Regular_Family_Addons").hide();
+    }
+    else if(this.value == 'Regular Family'){
+      $("#Regular_Family_Addons").slideDown();
+      $("#EZ_Family_Addons").hide();
+
+    }
+    else{
+      $("#EZ_Family_Addons").hide();
+      $("#Regular_Family_Addons").hide();
+    }
+  });
+
+  // Pet addon functionality
+  $("#pet_package_dropdown").on('change', function(){
+    if(this.value == 'EZ Pet'){
+      $("#EZ_Pet_Addons").slideDown();
+      $("#Regular_Pet_Addons").hide();
+    }
+    else if(this.value == 'Regular Pet'){
+      $("#Regular_Pet_Addons").slideDown();
+      $("#EZ_Pet_Addons").hide();
+
+    }
+    else{
+      $("#EZ_Pet_Addons").hide();
+      $("#Regular_Pet_Addons").hide();
+    }
+  });
 
   // Open up the addon drop down menus for "Move out clean"
   var expanded = false;
@@ -118,7 +144,7 @@ $(document).ready(function() {
     }
   });
 
-  // open up the addon drop down menus for "Little bit clean"
+  // Open up the addon drop down menus for "Little bit clean"
   $(".selectBox").on('click', function(){
     var checkboxes = document.getElementById("checkboxes");
     if (!expanded) {
@@ -130,11 +156,16 @@ $(document).ready(function() {
     }
   });
 
-  //Limit "Little bit clean" options to 3 only
+  // Limit "Little bit clean" options to 3 only
   $('input[type=checkbox]').on('change', function (e) {
     if ($('input[type=checkbox].lbc:checked').length > 3) {
         $(this).prop('checked', false);
     }
   });
 
+  // Price update for addon checkbox
+  $('input[type="checkbox"]').change(function(){
+    
+
+  });
 });
